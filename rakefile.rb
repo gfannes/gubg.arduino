35a6ee2288ba006fd3370a6741bbe512fe3f4f40
Geert Fannes <geert.fannes@gmail.com>
2018-05-12 16:23:10 +0200

    Added recipes.chai

--- rakefile.rb
+++ rakefile.rb
@@ -12,28 +12,9 @@ task :clean do
 end
 
 task :prepare do
-    build_ok_fn = 'gubg.build.ok'
-
-    Dir.chdir(shared_dir('extern')) do
-        if false
-            git_clone('https://github.com/arduino', 'Arduino') do
-                if !File.exist?(build_ok_fn)
-                    puts("Installing arduino")
-                    # sh 'rm -rf build'
-                    # sh "make -j 8 CMAKE_EXTRA_FLAGS=\"-DCMAKE_INSTALL_PREFIX:PATH=#{shared('install', 'neovim')}\""
-                    # sh 'make install'
-                    # Dir.chdir(shared('install', 'neovim', 'bin')) do
-                    #     publish('nvim', dst: 'bin', mode: 0755)
-                    # end
-                    # sh "touch #{build_ok_fn}"
-                end
-            end
-        else
-            unless File.exist?('Arduino-master')
-                zip_fn = 'master.zip'
-                sh "wget https://github.com/arduino/Arduino/archive/#{zip_fn}" unless File.exist?(zip_fn)
-                sh "unzip #{zip_fn}" 
-            end
+    Dir.chdir(GUBG::mkdir('extern')) do
+        unless File.exist?('ArduinoCore-avr')
+            sh "git clone --depth 1 https://github.com/arduino/ArduinoCore-avr"
         end
     end
 end
