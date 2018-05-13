4481d1fab643013253785018727f2ebd3a2c0b32
Geert Fannes <geert.fannes@gmail.com>
2018-05-13 16:27:07 +0200

    Added lilypad support

--- src/arduino.rb
+++ src/arduino.rb
@@ -11,12 +11,14 @@ module Arduino
             Rake.sh "avr-objcopy -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0 #{exe_fn} #{eep_fn}"
             Rake.sh "avr-objcopy -O ihex -R .eeprom #{exe_fn} #{hex_fn}"
             arch = (na[:arch]||:uno).to_sym
-            partno = case arch
-                     when :mega then "atmega2560"
-                     else raise("Unknown arch #{arch}") end
+            programmer, partno, baudrate = nil
+            case arch
+            when :mega    then programmer, partno, baudrate = "wiring", "atmega2560", 115200
+            when :lilypad then programmer, partno, baudrate = "avr109", "atmega32u4", 57600
+            else raise("Unknown arch #{arch}") end
             tty_fn = "/dev/ttyACM0"
             raise("TTY #{tty_fn} does not exist") unless File.exist?(tty_fn)
-            Rake.sh "avrdude -C//etc/avrdude.conf -v -p#{partno} -cwiring -P#{tty_fn} -b115200 -D -Uflash:w:#{hex_fn}:i"
+            Rake.sh "avrdude -C//etc/avrdude.conf -v -p#{partno} -c#{programmer} -P#{tty_fn} -b#{baudrate} -D -Uflash:w:#{hex_fn}:i"
         end
 
         puts("<< Programming #{exe_fn}")
