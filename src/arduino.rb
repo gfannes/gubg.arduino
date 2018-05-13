34c8f91ee073fb44708f656fdf73c7a48bda8467
Geert Fannes <geert.fannes@gmail.com>
2018-05-13 16:55:48 +0200

    reworked settings for avrdude a btbit

--- src/arduino.rb
+++ src/arduino.rb
@@ -10,14 +10,18 @@ module Arduino
             hex_fn = File.join(dir, "output.hex")
             Rake.sh "avr-objcopy -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0 #{exe_fn} #{eep_fn}"
             Rake.sh "avr-objcopy -O ihex -R .eeprom #{exe_fn} #{hex_fn}"
+
             arch = (na[:arch]||:uno).to_sym
-            programmer, partno, baudrate = nil
-            case arch
-            when :mega    then programmer, partno, baudrate = "wiring", "atmega2560", 115200
-            when :lilypad then programmer, partno, baudrate = "avr109", "atmega32u4", 57600
-            else raise("Unknown arch #{arch}") end
+            settings = {
+                mega:    {programmer: "wiring", partno: "atmega2560", baudrate: 115200},
+                lilypad: {programmer: "avr109", partno: "atmega32u4", baudrate: 57600},
+            }[arch]
+            raise("Could not find avrdude settings for arch \"#{arch}\"") unless settings
+
+            programmer, partno, baudrate = *[:programmer, :partno, :baudrate].map{|sym|settings[sym]}
             tty_fn = "/dev/ttyACM0"
             raise("TTY #{tty_fn} does not exist") unless File.exist?(tty_fn)
+
             Rake.sh "avrdude -C//etc/avrdude.conf -v -p#{partno} -c#{programmer} -P#{tty_fn} -b#{baudrate} -D -Uflash:w:#{hex_fn}:i"
         end
 
