54b221d76fa9e430ae1e80673988b552bde2141f
Geert Fannes <geert.fannes@auro-technologies.com>
2018-05-23 15:16:43 +0200

    wip

--- rakefile.rb
+++ rakefile.rb
@@ -1,6 +1,5 @@
-require(File.join(ENV['gubg'], 'shared'))
-require('gubg/build/Library')
-include GUBG
+require_relative("../gubg.build/bootstrap.rb")
+require("gubg/shared")
 
 task :default do
     sh "rake -T"
@@ -21,32 +20,4 @@ task :prepare do
             sh "git clone --depth 1 https://github.com/arduino-libraries/Servo"
         end
     end
-end
-
-#sudo apt install gcc-avr avr-libc avrdude
-task :run do
-    archs = [:lilypad, :uno]
-    # archs = [:uno]
-    archs.each do |arch|
-        lib = Build::Library.new('arduino-core', arch: arch)
-        puts("#{lib.lib_filename}")
-        avr_dir = shared('extern/Arduino-master/hardware/arduino/avr')
-        cpp_parts = %w[main]
-        c_parts = %w[wiring hooks wiring_digital]
-        case arch
-        when :lilypad
-            cpp_parts += %w[USBCore PluggableUSB CDC HardwareSerial HardwareSerial0 HardwareSerial1 HardwareSerial2 HardwareSerial3 IPAddress Print Stream Tone WMath WString abi new] 
-            c_parts += %w[WInterrupts wiring_analog wiring_pulse wiring_shift]
-        end
-        lib.add_sources(cpp_parts.map{|part|File.join(avr_dir, "cores/arduino/#{part}.cpp")})
-        lib.add_sources(c_parts.map{|part|File.join(avr_dir, "cores/arduino/#{part}.c")})
-        lib_dir = shared('extern/Arduino-master/libraries')
-        servo_cpp = File.join(lib_dir, "Servo/src/avr/Servo.cpp")
-        lib.add_sources(servo_cpp) if File.exist?(servo_cpp)
-        lib.build
-        publish(lib.lib_filename, dst: "lib/#{arch}")
-    end
-end
-
-task :test do
 end
--- readme.md
+++ readme.md
@@ -3,6 +3,12 @@ gubg.arduino
 
 Arduino interaction
 
+# Prerequisites
+
+## Ubuntu
+
+`sudo apt install gcc-avr avr-libc avrdude`
+
 # License
 
 This software is licensed under the EUPL v1.1 license with the explicit interpretation of the term _modification_ as explained in [license.txt](license.txt).
