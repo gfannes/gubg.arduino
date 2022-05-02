b0061450353a27af1b675a3e0c2f3b56dd9871a5
Geert Fannes <geert.fannes@gmail.com>
2022-05-03 01:03:18 +0200

    Added rake.rb

---
+++ rake.rb
@@ -0,0 +1,16 @@
+require("gubg/shared")
+
+here = File.dirname(__FILE__)
+
+namespace :arduino do
+	task :prepare do
+		Dir.chdir(GUBG::mkdir(here, 'extern')) do
+			unless File.exist?('ArduinoCore-avr')
+				sh "git clone --depth 1 https://github.com/arduino/ArduinoCore-avr"
+			end
+			unless File.exist?('Servo')
+				sh "git clone --depth 1 https://github.com/arduino-libraries/Servo"
+			end
+		end
+	end
+end
